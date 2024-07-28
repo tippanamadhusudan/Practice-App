27366856f481c8ac4eff1d76c8ef396fd74697ed
Madhu sudhan Reddy <tippanamadhusudan@gmail.com>
2024-07-28 15:00:57 -0400

    Adding AWS amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "practiceappa0365858": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
